2b892dd6a0fce941e6bfca5b3a9f838112f5c197
map <76199586+Maploop@users.noreply.github.com>
2024-10-21 10:02:29 +0330

    structs

--- main.go
+++ main.go
@@ -9,23 +9,52 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type Sprite struct {
+	Img  *ebiten.Image
+	X, Y float64
+}
+
+type Entity struct {
+	*Sprite
+
+	ChasePlayer bool
+}
+
 type Game struct {
-	PlayerImage *ebiten.Image
-	X, Y        float64
+	player   *Sprite
+	sprites  []*Sprite
+	entities []*Entity
 }
 
 func (g *Game) Update() error {
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		g.Y -= 2
+		g.player.Y -= 2
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		g.Y += 2
+		g.player.Y += 2
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		g.X -= 2
+		g.player.X -= 2
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		g.X += 2
+		g.player.X += 2
+	}
+
+	for _, sprite := range g.entities {
+		if !sprite.ChasePlayer {
+			continue
+		}
+		if sprite.X < g.player.X {
+			sprite.X += 1
+		} else if sprite.X > g.player.X {
+			sprite.X -= 1
+		}
+
+		if sprite.Y < g.player.Y {
+			sprite.Y += 1
+		} else if sprite.Y > g.player.Y {
+			sprite.Y -= 1
+		}
 	}
 
 	return nil
@@ -35,12 +64,34 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{120, 180, 255, 255})
 
 	opts := ebiten.DrawImageOptions{}
-	opts.GeoM.Translate(g.X, g.Y)
+	opts.GeoM.Translate(g.player.X, g.player.Y)
 
 	screen.DrawImage(
-		g.PlayerImage.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image),
+		g.player.Img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image),
 		&opts,
 	)
+
+	opts.GeoM.Reset()
+
+	for _, sprite := range g.sprites {
+		opts.GeoM.Translate(sprite.X, sprite.Y)
+		screen.DrawImage(
+			sprite.Img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image),
+			&opts,
+		)
+
+		opts.GeoM.Reset()
+	}
+
+	for _, sprite := range g.entities {
+		opts.GeoM.Translate(sprite.X, sprite.Y)
+		screen.DrawImage(
+			sprite.Img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image),
+			&opts,
+		)
+
+		opts.GeoM.Reset()
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -49,15 +100,29 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowTitle("CLERIA REMAKE")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
 	playerImg, _, err := ebitenutil.NewImageFromFile("assets/images/ninja.png")
 	if err != nil {
 		log.Fatal(err)
 	}
+	skeletonImg, _, err := ebitenutil.NewImageFromFile("assets/images/skeleton.png")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	if err := ebiten.RunGame(&Game{PlayerImage: playerImg, X: 100, Y: 100}); err != nil {
+	if err := ebiten.RunGame(&Game{
+		player: &Sprite{
+			Img: playerImg, X: 50, Y: 50,
+		},
+		entities: []*Entity{
+			{
+				&Sprite{Img: skeletonImg, X: 30, Y: 30},
+				true,
+			},
+		},
+	}); err != nil {
 		log.Fatal(err)
 	}
 }
